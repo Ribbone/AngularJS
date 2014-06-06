20c58a47990d069095ec4af88f46926db167e261
Risto Turtiainen <risto.turtiainen@metropolia.fi>
2014-06-07 01:06:09 +0300

    Lisatty serviceja & lisatty ulkoasu paranteluja & muokattu save/reset toimintaa

--- WebContent/exalted/js/controllers.js
+++ WebContent/exalted/js/controllers.js
@@ -1,32 +1,69 @@
 /* Controllers */
 
+var exaltedAppController = angular.module('exaltedAppController',
+		[ 'exaltedAppServices' ]);
+var charName;
+exaltedAppController.controller('hahmoController', function($scope, myService, mySecondService,myThirdService) {
 
-var exaltedAppController = angular.module('exaltedAppController', ['exaltedAppServices']);
+	myService.getFoos(charName).then(function(foos) {
 
-exaltedAppController.controller('hahmoController', function($scope, myService) {
-	myService.getFoos().then(function(foos) {
-        $scope.character = foos.exaltedCharacter;
-        $scope.master = $scope.character;
-        
-        $scope.update = function(user) {
-          $scope.master = angular.copy($scope.character);
-        };
+		/*
+		 * if (charName == {}) { $scope.master = {}; } else {
+		 */
+		$scope.character = foos.exaltedCharacter;
+		$scope.master = angular.copy($scope.character);
+		// }
+	});
 
-        $scope.reset = function() {
-          $scope.character = angular.copy($scope.master);
-        };
+	$scope.update = function(character) {
+		$scope.master = angular.copy(character);
+		myThirdService.postFoos(character.name , character);
 
-        $scope.reset();
-      
-    });
-	
-  });
+	};
 
-  exaltedAppController.controller('welcomeController', ['$scope',
-  function($scope) {
-    $scope.hahmot = [
-		{name: 'Risto', characterClass: 'Warrior'},
-		{name: 'Petteri', characterClass: 'Paladin'},
-		{name: 'Lari', characterClass: 'Priest'}
-	]
-  }]);
+	$scope.reset = function() {
+		$scope.character = angular.copy($scope.master);
+	};
+});
+
+exaltedAppController.controller('welcomeController',
+		function($scope, myService) {
+
+			/*
+			 * mySecondService.getChars().then(function(foos) {
+			 * 
+			 * $scope.hahmot = foos.hahmotaulukko; //TODO Hahmotaulukkon alustus
+			 * 
+			 * 
+			 * });
+			 */
+
+			$scope.hahmot = [ {
+				name : 'asd',
+				characterClass : 'lolladin'
+			},{
+				name : 'Groe',
+				characterClass : 'Warrior'
+			}, {
+				name : 'Risto',
+				characterClass : 'Warrior'
+			}, {
+				name : 'Petteri',
+				characterClass : 'Paladin'
+			}, {
+				name : 'Lari',
+				characterClass : 'Priest'
+			} ]
+
+			$scope.newCharacter = function() {
+				console.log('uusi characteri');
+				charName = {};
+			}
+
+			$scope.selectCharacter = function(char) {
+				console.log('valittu character:');
+				console.log(char);
+				charName = angular.copy(char);
+			}
+
+		});
